0d565ad819e75cce96922b5a8073e5872eca05f0
Gocha Ossinkine <ossinkine@ya.ru>
2016-05-23 18:35:11 +0500

    Remove ResponseListener to avoid headers replacement

--- src/Application.php
+++ src/Application.php
@@ -4,6 +4,9 @@ use Controller\ProxyController;
 use GuzzleHttp\Client as HttpClient;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpKernel\EventListener\ResponseListener;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class Application extends Silex\Application
 {
@@ -22,6 +25,19 @@ class Application extends Silex\Application
         });
 
         parent::__construct($values);
+
+        // Remove ResponseListener to avoid headers replacement
+        $this['dispatcher'] = $this->extend('dispatcher', $this->share(function (EventDispatcherInterface $dispatcher) {
+            $listeners = $dispatcher->getListeners(KernelEvents::RESPONSE);
+            foreach ($listeners as $listener) {
+                list($object, $method) = $listener;
+                if ($object instanceof ResponseListener) {
+                    $dispatcher->removeListener(KernelEvents::RESPONSE, $listener);
+                }
+            }
+
+            return $dispatcher;
+        }));
 
 //        $this->register(new TwigServiceProvider(), [
 //            'twig.path'    => __DIR__.'/../views',
